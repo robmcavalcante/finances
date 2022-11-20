ed464e841f2e834b74691007f414b640c5f1e5fb
Robson Machado Cavalcante <robmcavalcante@gmail.com>
2022-11-20 16:10:46 -0400

    feat: allow adding new credit cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,24 @@ class CardsController < ApplicationController
   def index
     @pagy, @cards = pagy(Card.all, items: 10)
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      redirect_to cards_path, notice: 'Cartão adicionado com sucesso!'
+    else
+      redirect_to cards_path, alert: 'Erro ao tentar salvar o cartão. Tente novamente!'
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:description, :flag, :limit, :closure, :expiration)
+  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,7 @@
 class Card < ApplicationRecord
+  validates :description, presence: true, length: { maximum: 30 }
+  validates :flag, presence: true, length: { maximum: 20 }
+  validates :limit, presence: true
+  validates :closure, presence: true, length: { maximum: 2 }
+  validates :expiration, presence: true, length: { maximum: 2 }
 end
