798c2688d82bbcf9da3fc002572071b05bc83634
Robson Machado Cavalcante <robmcavalcante@gmail.com>
2022-11-21 16:02:17 -0400

    feat: allow adding new categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,27 @@
 class CategoriesController < ApplicationController
+  before_action :set_user, only: %i[ edit update ]
+
   def index
     @pagy, @categories = pagy(Category.all, items: 10)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_path, notice: 'Categoria adicionada com sucesso!'
+    else
+      redirect_to categories_path, alert: 'Erro ao salvar a nova categoria. Tente novamente!'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon, :color)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :icon, presence: true, length: { maximum: 75 }
+  validates :color, presence: true, length: { maximum: 20 }
 end
