9c67bcea32b13ad65afecd0409a2c66a4e505dcc
Robson Machado Cavalcante <robmcavalcante@gmail.com>
2022-11-29 06:39:27 -0400

    feat: allow registering income receipts

--- app/controllers/revenues_controller.rb
+++ app/controllers/revenues_controller.rb
@@ -2,4 +2,24 @@ class RevenuesController < ApplicationController
   def index
     @pagy, @revenues = pagy(Revenue.all, items: 6)
   end
+
+  def new
+    @revenue = Revenue.new
+  end
+
+  def create
+    @revenue = Revenue.new(revenue_params)
+
+    if @revenue.save
+      redirect_to revenues_path, notice: 'Receita adicionado com sucesso!'
+    else
+      redirect_to revenues_path, alert: 'Erro ao salvar a receita. Tente novamente!'
+    end
+  end
+
+  private
+
+  def revenue_params
+    params.require(:revenue).permit(:description, :value)
+  end
 end
