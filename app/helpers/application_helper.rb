9e1251a40394fc8c1388bdcf2a57115024ba8763
Robson Machado Cavalcante <robmcavalcante@gmail.com>
2022-11-28 20:43:19 -0400

    feat: configure active link style in sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
   def current_date
     Date.today.strftime("%d/%m/%Y")
   end
+
+  def active_link(value)
+    if request.fullpath.include?("#{value}")
+      "active"
+    end
+  end
 end
