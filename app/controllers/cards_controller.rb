23b7b367d6e30d33ce34d2c75eb0d5eb3e49eb51
Robson M. Cavalcante <robmcavalcante@gmail.com>
2022-12-04 09:22:06 -0400

    feat: allow creating, viewing, editing and deleting cards (CRUD)

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,50 @@
+class CardsController < ApplicationController
+  before_action :set_user, only: %i[ edit update destroy ]
+
+  def index
+    @pagy, @cards = pagy(Card.all, items: 10)
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      redirect_to cards_path, notice: 'Cartão cadastrado com sucesso!'
+    else
+      redirect_to cards_path, alert: 'Ocorreu um erro no cadastro do cartão. Tente novamente!'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @card.update(card_params)
+      redirect_to cards_path, notice: 'Cartão atualizado com sucesso!'
+    else
+      redirect_to cards_path, alert: 'Ocorreu um erro atualizar os dados do cartão. Tente novamente!'
+    end
+  end
+
+  def destroy
+    if @card.destroy
+      redirect_to cards_path, notice: 'Cartão excluído com sucesso!'
+    else
+      redirect_to cards_path, alert: 'Ocorreu um erro ao remover o cartão. Tente novamente!'
+    end
+  end
+
+  private
+
+  def set_user
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:description, :flag, :limit, :closure, :expiration)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: redirect('/dashboard')
   get '/dashboard', to: 'dashboard#index', as: 'dashboard'
+
+  resources :cards
 end
